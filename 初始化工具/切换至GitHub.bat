c5d97700d1d70addf391f19162fe5e37c977558a
pear-studio <pear_studio@163.com>
2022-02-15 23:43:22 +0800

    Update Gitee&GitHub bat

--- 初始化工具/切换至GitHub.bat
+++ 初始化工具/切换至GitHub.bat
@@ -4,9 +4,9 @@ echo Switch to Gitee ...
 
 set GIT_EXE=.\PortableGit\bin\git.exe
 
-set DPP_GITEE_PATH=https://gitee.com/pear_studio/nonebot-dicepp.git
-set DPP_PY_GITEE_PATH=https://gitee.com/pear_studio/dicepp-python-win64.git
-set DPP_TOOL_GITEE_PATH=https://gitee.com/pear_studio/dicepp-tools-win64.git
+set DPP_GITEE_PATH=https://github.com/pear-studio/nonebot-dicepp.git
+set DPP_PY_GITEE_PATH=https://github.com/pear-studio/dicepp-python-win64.git
+set DPP_TOOL_GITEE_PATH=https://github.com/pear-studio/dicepp-tools-win64.git
 
 echo Change Tool Repo
 cd ..
